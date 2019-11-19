8f3ea0613b33db18886788d5707c42c5d030b1ad
Raquel <rgg242@inlumine.ual.es>
2019-11-19 19:24:27 +0100

    avance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Inicio from "./inicio/inicio"
 import Menu from "./common/menu/movil/menu"
+import Galeria from "./galeria/galeria"
+
 
 function App() {
     return (
         //<Menu/>
-        <Inicio/>
+        //<Inicio/>
+        <Galeria/>
     );
 }
 
--- src/galeria/galeria.js
+++ src/galeria/galeria.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
-import Gallery from "react-photo-gallery";
-import Photo from "./Photo";
-import arrayMove from "array-move";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
-import { photos } from "./photos";
+import React from 'react';
+//import fondo from "./fondo.jpg";
+//import Movil from "./movil/inicio";
+//import Ordenador from "./ordenador/inicio";
+import Imagen_fondo from "../common/imagen_fondo/imagen_fondo"
+import "./galeria.scss";
 
-/* popout the browser and maximize to see more rows! -> */
-const SortablePhoto = SortableElement(item => <Photo {...item} />);
-const SortableGallery = SortableContainer(({ items }) => (
-  <Gallery photos={items} renderImage={props => <SortablePhoto {...props} />} />
-));
+const imgWithClick = { cursor: "pointer" };
 
-function App() {
-  const [items, setItems] = useState(photos);
+const Galeria = ({ index, onClick, photo, margin, direction, top, left }) => {
+  const imgStyle = { margin: margin };
+  if (direction === "column") {
+    imgStyle.position = "absolute";
+    imgStyle.left = left;
+    imgStyle.top = top;
+  }
 
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    setItems(arrayMove(items, oldIndex, newIndex));
+  const handleClick = event => {
+    onClick(event, { photo, index });
   };
 
   return (
-    <div>
-      <h2>Sortable Gallery</h2>
-      <h3>Drag photo to rearrange</h3>
-      <SortableGallery items={items} onSortEnd={onSortEnd} axis={"xy"} />
-    </div>
+    <img
+      style={onClick ? { ...imgStyle, ...imgWithClick } : imgStyle}
+      {...photo}
+      onClick={onClick ? handleClick : null}
+      alt="img"
+    />
   );
-}
-render(<App />, document.getElementById("app"));
+};
+
+export default Galeria;
