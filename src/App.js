d5bed2c22dd382f35c0da677ace432229e3a20fa
jmg595@inlumine.ual.es <jmg595@inlumine.ual.es>
2019-11-24 01:25:37 +0100

    Contactos, falta formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Inicio from "./inicio/inicio"
 import Precios from "./precios/precios"
+import Contacto from "./contacto/contacto"
 
 function App() {
     return (
         //<Menu/>
         //<Inicio/>
         //<Galeria/>
-        <Precios/>
+        //<Precios/>
+        <Contacto/>
     );
 }
 
