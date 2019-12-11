766a9a3d339b2db5955f83dc1a5987584feec145
jmg595@inlumine.ual.es <jmg595@inlumine.ual.es>
2019-12-11 12:38:48 +0100

    Admin contacto modificable

--- src/contacto/contacto.js
+++ src/contacto/contacto.js
@@ -9,6 +9,7 @@ import bodasnet from "./iconos/bodasnet.svg"
 import f_black from "./iconos/facebook-black.svg";
 import e_black from "./iconos/email-black.svg";
 import w_black from "./iconos/whatsapp-black.svg"
+import traduccion from "../traduccion/es/common.json"
 import "./contacto.scss";
 
 // Traducción
@@ -31,6 +32,10 @@ i18next.init({
 });*/
 
 function Contacto() {
+
+    const admin = true;
+    const json = traduccion.contacto;
+
     //const { t, i18n } = this.props;
     return (
         <div className = "content-contacto">
@@ -47,43 +52,51 @@ function Contacto() {
                                     <th colSpan="3">
                                         {//<h1>{this.props.t('contacto.titulo', { framework: "react-i18next" })}</h1>
                                         }
-                                        <h1>LOGAR FOTÓGRAFOS</h1>
+                                        <h1 className={admin ? 'none' : null}>{json.titulo}</h1>
+                                        <input type="text" name="titulo" defaultValue={json.titulo} className={admin ? 'admin input-title' : 'none'}/>
                                     </th>
                                 </tr>
                                 <tr>
                                     <th colSpan="3">
-                                        <h3>FOTÓGRAFOS PROFESIONALES</h3>
+                                        <h3 className={admin ? 'none' : null}>{json.subtitulo}</h3>
+                                        <input type="text" name="subtitulo" defaultValue={json.subtitulo} className={admin ? 'admin input-subtitle' : 'none'}/>
                                     </th>
                                 </tr>
                                 <tr>
                                     <td><img src = {ubicacion} className = "icono"/></td>
                                     <td className = "titulo">DIRECCIÓN</td>
-                                    <td className = "datos">C/SOL Nº1, BERJA, ALMERÍA</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.direccion}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.direccion} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                                 <tr>
                                     <td><img src = {correo} className = "icono"/></td>
                                     <td className = "titulo">CORREO</td>
-                                    <td className = "datos">LOGARFOTOGRAFOS@LIVE.ES</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.correo}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.correo} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                                 <tr>
                                     <td><img src = {telefono} className = "icono"/></td>
                                     <td className = "titulo">TELÉFONO</td>
-                                    <td className = "datos">(+34) 689 13 77 01</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.telefono}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.telefono} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                                 <tr>
                                     <td><img src = {whatsapp} className = "icono"/></td>
                                     <td className = "titulo">WHATSAPP</td>
-                                    <td className = "datos">(+34) 609 41 04 71</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.whatsapp}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.whatsapp} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                                 <tr>
                                     <td><img src = {facebook} className = "icono"/></td>
                                     <td className = "titulo">FACEBOOK</td>
-                                    <td className = "datos">LOGAR FOTÓGRAFOS</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.facebook}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.facebook} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                                 <tr>
                                     <td><img src = {bodasnet} className = "icono"/></td>
                                     <td className = "titulo">BODAS.NET</td>
-                                    <td className = "datos">LOGAR FOTÓGRAFOS</td>
+                                    <td className = {admin ? 'none' : 'datos'}>{json.tabla.bodasNet}</td>
+                                    <td><input type="text" name="direccion" defaultValue={json.tabla.bodasNet} className={admin ? 'admin input-data' : 'none'}/></td>
                                 </tr>
                             </table>
                         </div>
@@ -99,6 +112,7 @@ function Contacto() {
                 
 
                 <div className = "info-contacto-formulario sombra">
+                    <div className="cuadro-giro-2-3 sombra"></div>
                     <div className = "info-contacto-formulario-contenido">
                         <h1>ENVIAR UN MENSAJE</h1>
                         <form>
